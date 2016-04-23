fd5b08815f10e90367931423051334d48b016a07
SimonHylander <simon.hylander@docu.se>
2016-04-24 01:26:42 +0200

    .¨

--- js/app.js
+++ js/app.js
@@ -1,8 +1,44 @@
 'use strict';
 
 var app = angular.module('chatApp', []);
-app.controller('chatController', function($scope){
+
+app.factory('socket', function($rootScope){
+	var ws = new WebSocket("ws://127.0.0.1:8080");
+	ws.onmessage = function (event) {
+		var response;
+	    try {
+	        response = angular.fromJson(event.data);
+	    } catch (e) {
+	        //document.getElementById("helloId").innerHTML = "Sorry, connection failed ...";
+	        //document.getElementById("btnAtpId").disabled = false;
+	        console.log('error: ', e);
+	        response = {'error': e};
+	    }
+	    
+	    console.log(response);
+	    
+	    if(response.data) {}
+	};
+	
+	ws.onopen = function() {};
+	ws.onclose = function() {};
+	ws.onerror = function(error) {};
+	
+	return {
+		status: function() { return ws.readyState },
+		send: function(message) {
+			if(angular.isString(Message)) {
+				ws.send(message);
+			}else if(angular.isObject(message)){
+				ws.send(JSON.stringify(message));
+			}
+		}
+	};
+});
+
+app.controller('chatController', function($scope, socket) {
     $scope.sendMessage = function() {
         console.log('send message');
+        console.log(socket.status());
     }
 });
